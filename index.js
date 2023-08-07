03395ad9e905bb5c1cbc3f6bcba94c3f7a337f4c
fernando <isaacfernando5351@gmail.com>
2023-08-07 08:52:46 -0600

    feature :sparkles: server running

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { config } = require('./config');
 
 app.listen(app.get('port'), ()=> {
     try {
-        config.isProduction ? console.log(`Server is running on http://localhost:${app.get('port')}`) : console.log();;
+        config.isProduction ? console.log() : console.log(`Server is running on http://localhost:${app.get('port')}`);
     } catch (error) {
         console.log(`There was an error in: ${error}`);
     }
