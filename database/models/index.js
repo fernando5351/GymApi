9fde59b17d8aab0d12f726f2604f8399a7b3b51a
fernando <isaacfernando5351@gmail.com>
2023-08-08 10:47:45 -0600

    feature :sparkles: models management function

--- database/models/index.js
+++ database/models/index.js
@@ -1,7 +1,7 @@
-
+const { User, UserModel } = require('./user.model');
 
 function setupModels(sequelize) {
-    
+    User.init(UserModel, User.config(sequelize));
 }
 
 module.exports = setupModels;
