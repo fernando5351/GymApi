9479dc06b1b9e3a16eae014ed18d8b9a49325e46
fernando <isaacfernando5351@gmail.com>
2023-08-08 13:39:53 -0600

    feature :sparkles: using middlewares

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express =  require('express');
 const port = process.env.PORT || 300;
+const { logErrors, errorHanlder, ormErrorHandler, boomErrorHandler } = require('../middleware/errorhanlder');
 
 const app = express();
 
 app.set('port', port);
+
+app.use(logErrors);
+app.use(errorHanlder);
+app.use(ormErrorHandler);
+app.use(boomErrorHandler);
 
 module.exports = app;
