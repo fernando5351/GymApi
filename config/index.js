14b6fa21e05298b0b53a18a51c05e574eb41e0b8
fernando <isaacfernando5351@gmail.com>
2023-08-07 21:31:42 -0600

    feature :hammer: configuration for database connection

--- config/index.js
+++ config/index.js
@@ -2,7 +2,13 @@ require('dotenv').config();
 
 const config = {
     isProduction: process.env.environment === 'production',
-    db: {},
+    db: {
+        name: process.env.DATABASE,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_PORT,
+        port: process.env.DB_HOST,
+    },
 }
 
 module.exports = { config };
