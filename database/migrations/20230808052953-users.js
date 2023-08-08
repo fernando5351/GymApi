5515ea4cf903d7426d1fea8b4e7d0cf498746f0d
fernando <isaacfernando5351@gmail.com>
2023-08-08 10:46:02 -0600

    feature :sparkles: user voidplet migration

--- database/migrations/20230808052953-users.js
+++ database/migrations/20230808052953-users.js
@@ -1,22 +1,14 @@
 'use strict';
 
+const { USER_TABLE, UserModel } = require('../models/user.model');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up (queryInterface) {
+    await queryInterface.createTable(USER_TABLE, UserModel);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down (queryInterface) {
+    await queryInterface.dropTable(USER_TABLE);
   }
 };
